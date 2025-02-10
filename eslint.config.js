ab2a2a72939ea4230d58aaa9d00e2d7256fba670
李辰 <2095055676@qq.com>
2025-02-11 05:04:33 +0800

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginVue from 'eslint-plugin-vue';
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  { files: ['**/*.vue'], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'semi': ['warn', 'always'],
+      'no-console': 'warn',
+      'max-len': ['warn', { 
+        'code': 100,
+        'ignoreUrls': true,
+        'ignoreStrings': true,
+        // 自动换行
+        'ignoreComments': true,
+      }],
+      'indent': ['error', 2],
+      // 'comma-dangle': ['error', 'always-multiline'],
+      'quotes': ['error', 'single'],
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+      'comma-spacing': ['error', { 'before': false, 'after': true }],
+      // 不禁用any
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+];
